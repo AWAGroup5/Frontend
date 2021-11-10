a755028ed45b445ac23ea9b556c7bbaa20d54d1c
idRisto <risto.alanen@gmail.com>
2021-11-10 12:31:49 +0200

    Computer switch

--- src/components/Manager.js
+++ src/components/Manager.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react'
+import NavBar from './NavBar'
+import styles from './modules/Home.module.css'
 
 export default class Manager extends Component {
     render() {
         return (
             <div>
+                <NavBar />
+                <div className={ styles.spacer }></div>
                 <h1>Manager page</h1>
                 <p>This page needs Button to add new category, product and manage orders. Also all the products of this restaurant should be visible and editable</p>
             </div>
