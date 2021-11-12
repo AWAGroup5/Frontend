d5e5baebdf9b3b4582d521efaf16e994ef206359
unknown <risto.alanen@gmail.com>
2021-11-12 14:47:27 +0200

    Product creating page started

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './components/Home';
 import Manager from './components/Manager';
 import RegisterRestaurant from './components/RegisterRestaurant';
 import RegisterUser from './components/RegisterUser';
+import AddProduct from './components/AddProduct';
 
 class App extends React.Component {
   render() {
@@ -14,6 +15,7 @@ class App extends React.Component {
           <Route path="/resReg" element={ <RegisterRestaurant /> } />
           <Route path="/userReg" element={ <RegisterUser /> } />
           <Route path="restaurant" element={ <Manager /> } />
+          <Route path="restaurant/newproduct" element={ <AddProduct /> } />
         </Routes>
       </div>
     );
