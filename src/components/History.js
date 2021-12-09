a1cf495fddde432a36895e8d1025d72b570f14d1
c0latu02 <78533839+c0latu02@users.noreply.github.com>
2021-12-09 12:06:57 +0200

    Customer confirm orders and view order status done

--- src/components/History.js
+++ src/components/History.js
@@ -6,9 +6,10 @@ import styles from './modules/history.module.css'
 
 
 
-function History() {
+function History(props) {
+
     const [orders, setItems] = useState([]);
-   
+
 
     const decodedJwt = jwt.decode(window.localStorage.getItem('appAuthData'));
     let idcustomer = null;
@@ -18,10 +19,25 @@ function History() {
         idcustomer = null
     }
 
+    const onSubmit = (index) => {
+        let updatedOrder = {
+            idrestaurant: orders[index].idrestaurant,
+            idcustomer: orders[index].idcustomer,
+            status: "done",
+            cost: orders[index].cost,
+            address: orders[index].address
+            
+        }
+        console.log(updatedOrder)
 
-
-  
-
+        axios.put('https://awaproject5db.herokuapp.com/order/' + orders[index].idorder, updatedOrder)
+        .then(res => {
+            console.log(res.data)
+            window.location = '/history/';
+        }).catch(function(error) {
+            console.log(error)
+        })
+    }
 
     useEffect(() => {
         console.log(idcustomer)
@@ -48,9 +64,8 @@ function History() {
             // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
 
-    const orderHistory = orders.map((orders, index) => 
-    
-        {if(orders.status === "done"){
+    const orderHistory = orders.map((orders, index) => {
+        if(orders.status === "done"){
             return(
                 <div className={styles.order} key = {index }> 
                     <div className={styles.text}>OrderId: {orders.idorder}</div>
@@ -58,29 +73,42 @@ function History() {
                     <div className={styles.text}>Address: {orders.address}</div>
                     <div className={styles.text}>Status: {orders.status}</div>
                 </div>
-            )
-        }}
-    
-    )
+            )   
+        }
+    })
 
-
-
-    
-
-
-    
-        return (
-            <>
-                <div className={ styles.container }>
-                    <div>
-                        {
-                            orderHistory
-                        }
-                    </div>
+    const orderStatus = orders.map((orders, index) =>{
+        if(orders.status !== "done"){
+            return(
+                <div className={styles.order} key = {index }> 
+                    <div className={styles.text}>OrderId: {orders.idorder}</div>
+                    <div className={styles.text}>Price: {orders.cost} €</div>
+                    <div className={styles.text}>Address: {orders.address}</div>
+                    <div className={styles.text}>Status: {orders.status}</div>
+                    { orders.status === "delivered" ? 
+                     <button onClick={ ()=> onSubmit(index)  }> confirm </button>
+                    : null }
                 </div>
-            </>
-        )
+            )
+        }
+    })
     
+    return (
+        <>
+            <div className={ styles.container }>
+                <div><h1 className={ styles.header}>History</h1>
+                    {
+                        orderHistory
+                    }
+                </div>
+                <div><h1 className={ styles.header}>Pending orders</h1>
+                    {
+                        orderStatus
+                    }
+                </div>
+            </div>
+        </>
+    )  
 }
 
 
